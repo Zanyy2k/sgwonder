d07ecf6dcd783f6e73c0f1ef411bd3be83f95956
ZD <yuzongdongsg@hotmail.com>
2024-10-19 15:42:28 +0800

    add new views/components

--- src/router.js
+++ src/router.js
@@ -6,6 +6,8 @@ import About from "./views/About.vue";
 import Contact from "./views/Contact.vue";
 import Element from "./views/Element.vue";
 import Services from "./views/Services.vue";
+import Services_Batam from "./views/Services_Batam.vue";
+import Services_StudyTour from "./views/Services_StudyTour.vue";
 
 const routes = [
   { path: "/sgwonder/", name: "Home", component: Home },
@@ -13,6 +15,16 @@ const routes = [
   { path: "/sgwonder/contact", name: "Contact", component: Contact },
   { path: "/sgwonder/element", name: "Element", component: Element },
   { path: "/sgwonder/services", name: "Services", component: Services },
+  {
+    path: "/sgwonder/Services_StudyTour",
+    name: "Services_StudyTour",
+    component: Services_StudyTour,
+  },
+  {
+    path: "/sgwonder/services_batam",
+    name: "services_batam",
+    component: Services_Batam,
+  },
 ];
 
 const router = createRouter({
